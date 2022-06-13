350f402e1c8c32e01cf7e181faf3d52e18a2c122
Carlos Villatoro <crvb0797@gmail.com>
2022-06-13 09:15:51 -0600

    Creamos las validaciones para los campos de los formularios y me encargue de los valores repeditos para que no se puedan generar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,15 @@ class PostController extends Controller
         return view('posts.index', compact('posts'));
     }
 
+    public function create(Post $post){
+        return view('posts.create', ['post' => $post]);
+    }
+
     public function store(Request $request){
+        $request->validate([
+            'title' => 'required|unique:posts,title',
+            'body' => 'required',
+        ]);
         $post = $request->user()->posts()->create([
         'title' => $title = $request->title,
         'slug' => Str::slug($title),
@@ -23,16 +31,29 @@ class PostController extends Controller
         return redirect()->route('posts.edit', $post);
     }
 
+    public function edit(Post $post){
+        return view('posts.edit', ['post' => $post]);
+    }
+
+    public function update(Request $request, Post $post){
+        $request->validate([
+            'title' => 'required|unique:posts,title, ' . $post->id,
+            'body' => 'required',
+        ]);
+        $post->update([
+        'title' => $title = $request->title,
+        'slug' => Str::slug($title),
+        'body' => $request->body,
+        ]);
+        return redirect()->route('posts.edit', $post);
+    }
+
+
     public function destroy(Post $post){
         $post->delete ();
         return back();
     }
 
-    public function create(Post $post){
-        return view('posts.create', ['post' => $post]);
-    }
-
-    public function edit(Post $post){
-        return view('posts.edit', ['post' => $post]);
-    }
+    
+   
 }
--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,10 +1,17 @@
 @csrf
 
 <label class="uppercase text-gray-700 text-sm">Titulo</label>
-<input type="text" name="title" class="rounded border-gray-200 w-full mb-4" value="{{$post->title}}">
+<input type="text" name="title" class="rounded border-gray-200 w-full mb-4" value="{{old('title', $post->title)}}">
+<span class="text-sm text-red-500">@error('title') {{$message}} @enderror</span>
+
+<br>
+<br>
 
 <label class="uppercase text-gray-700 text-sm">Cuerpo de la publicación</label>
-<textarea name="body" rows="5" class="rounded border-gray-200 w-full mb-4">{{$post->body}}</textarea>
+<textarea name="body" rows="5" class="rounded border-gray-200 w-full mb-4">{{old('body',$post->body)}}</textarea>
+<span class="text-sm text-red-500">@error('body') {{$message}} @enderror</span>
+
+<br>
 
 <div class="flex items-center justify-between">
     <a href="{{route('posts.index')}}" class="text-indigo-600">Regresar</a>
