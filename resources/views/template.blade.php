9dac28b1ba99da9ea20a095210b90d53b38bb9ef
Carlos Villatoro <crvb0797@gmail.com>
2022-06-14 05:08:20 -0600

    Creamos los estilos para la barra de navegación, la barra separadora y el pie de página, además configuramos el logo de la página

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,23 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="shortcut icon" href="{{asset('img/logo.png')}}" type="image/x-icon">
     <title>Curso de Laravel 9</title>
 </head>
+
 <body>
-    <p>
-        <a href="{{route('home')}}">Home</a>
-        @auth
-        <a href="{{route('dashboard')}}">Dashboard</a>
-        @else
-        <a href="{{route('login')}}">Inicio de sesión</a>
-        @endauth
-    </p>
+    <div class="container px-4 mx-auto">
+        <header class="flex justify-between items-center py-4">
+            <div class="flex items-center flex-grow gap-4">
+                <a href="{{ route('home') }}">
+                    <img src="{{asset('img/logo.png')}}" class="h-12">
+                </a>
+                <form action="">
+                    <input type="text" placeholder="Buscar...">
+                </form>
+            </div>
 
-    <hr>
+            @auth
+                <a href="{{ route('dashboard') }}">Dashboard</a>
+            @else
+                <a href="{{ route('login') }}">Inicio de sesión</a>
+            @endauth
+        </header>
 
-    @yield('content')
+        <div class="opacity-60 h-px mb-8" style="
+            background: linear-gradient(to right,
+            rgba(200,200,200,0) 0%,
+            rgba(200,200,200,1) 30%,
+            rgba(200,200,200,1) 70%,
+            rgba(200,200,200,0) 100%
+            );
+         ">
+
+        </div>
+        @yield('content')
+
+        <p class="py-16">
+            <img src="{{asset('img/logo.png')}}" class="h-12 mx-auto">
+        </p>
+    </div>
 </body>
 </html>
